9ed8233d30a3b2493764e02b1f322a639f8caa41
Neil Crothers <nkcrothers@gmail.com>
2019-12-20 19:53:15 -0700

    data loaded to database

--- tables.sql
+++ tables.sql
@@ -1,26 +1,26 @@
 CREATE DATABASE sdc_products;
 
 CREATE TABLE IF NOT EXISTS product_info 
-    (user_id serial PRIMARY KEY,
-    name VARCHAR (25) UNIQUE NOT NULL,
-    slogan VARCHAR (100) NOT NULL,
-    description VARCHAR (250) NOT NULL,
+    (product_id serial PRIMARY KEY,
+    name VARCHAR (100) NOT NULL,
+    slogan TEXT NOT NULL,
+    description TEXT NOT NULL,
     category VARCHAR (25) NOT NULL,
     default_price integer NOT NULL);
 
 CREATE TABLE IF NOT EXISTS styles
-    (user_id integer REFERENCES product_info(user_id),
+    (product_id integer REFERENCES product_info(product_id),
     style_id serial PRIMARY KEY,
-    name VARCHAR (100) UNIQUE NOT NULL,
+    name VARCHAR (100) NOT NULL,
     original_price integer NOT NULL,
-    sale_price integer,
+    sale_price VARCHAR (10),
     is_default integer NOT NULL);
 
 CREATE TABLE IF NOT EXISTS photos 
     (style_id integer REFERENCES styles(style_id),
     photos_id integer UNIQUE NOT NULL,
-    thumbnail_url VARCHAR (100),
-    url VARCHAR (100));
+    thumbnail_url TEXT,
+    url TEXT);
 
 CREATE TABLE IF NOT EXISTS skus 
     (style_id integer REFERENCES styles(style_id),
@@ -28,10 +28,10 @@ CREATE TABLE IF NOT EXISTS skus
     size VARCHAR (10),
     quantity integer);
 
-COPY product_info FROM '/Users/neilcrothers/ghrbld06/Data/products.csv' DELIMITER ',' CSV HEADER;
+COPY product_info FROM '/Users/neilcrothers/ghrbld06/Data/product.csv' DELIMITER ',' CSV HEADER;
 
--- COPY styles FROM '/Users/neilcrothers/ghrbld06/Data/styles.csv' DELIMITER ',' CSV HEADER;
+COPY styles(style_id, product_id, name, sale_price, original_price, is_default) FROM '/Users/neilcrothers/ghrbld06/Data/styles.csv' DELIMITER ',' CSV HEADER;
 
--- COPY photos FROM '/Users/neilcrothers/ghrbld06/Data/photos.csv' DELIMITER ',' CSV HEADER;
+COPY photos(photos_id, style_id, url, thumbnail_url) FROM '/Users/neilcrothers/ghrbld06/Data/photos.csv' DELIMITER ',' CSV HEADER;
 
--- COPY skus(skus_id, style_id, size, quantity) FROM '/Users/neilcrothers/ghrbld06/Data/skus.csv' DELIMITER ',' CSV HEADER;
+COPY skus(skus_id, style_id, size, quantity) FROM '/Users/neilcrothers/ghrbld06/Data/skus.csv' DELIMITER ',' CSV HEADER;
