70b2eef302e5a49da37ac9c8f7edbbfa99c87343
Neil Crothers <nkcrothers@gmail.com>
2019-12-30 15:34:11 -0700

    .env file should be set up

---
+++ server/routes.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    port: process.env.PGPORT
+})
